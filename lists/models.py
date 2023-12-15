b8c43b9c680c1f3c52f0645cdbe7f4484c8869e0
Marcos Werle <marcoskorbeswerle@hotmail.com>
2023-12-15 15:10:59 -0300

    Add Menu app and configure Model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class List(models.Model):
+    title = models.CharField(
+        max_length=100)
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.CASCADE,
+        related_name='status',
+        to_field='id',
+        default='1')
+
+    def __str__(self):
+        return self.title
